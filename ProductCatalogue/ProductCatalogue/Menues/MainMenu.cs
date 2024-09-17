ff3e808efcf214965db10c8bd0ea2575a8286647
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-09-17 08:20:15 +0200

    Started main menu
    Started main menu

---
+++ ProductCatalogue/ProductCatalogue/Menues/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ProductCatalogue.Menues;
+
+internal static class MainMenu
+{
+    public static void DisplayMenu()
+    {   
+        Console.Clear();
+        Console.WriteLine("Menu;");
+        Console.WriteLine("1. Show product list.");
+        Console.WriteLine("2. Add a product.");
+        Console.WriteLine("3. Change a product.");
+        Console.WriteLine("4. Remove a product.");
+        Console.WriteLine("5. Add old text file of products into program.");
+        Console.WriteLine("0. Exit program.");
+        Console.Write("Enter an option; ");
+
+        ProductMenu.ProductMenuChoice();
+
+    }
+}
