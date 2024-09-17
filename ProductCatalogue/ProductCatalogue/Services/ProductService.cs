9f98a962891616206a4f010355e49b9862ea44de
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-09-17 08:19:17 +0200

    Product service.
    Started product service.

---
+++ ProductCatalogue/ProductCatalogue/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using ProductCatalogue.Models;
+
+namespace ProductCatalogue.Services;
+
+
+//CRUD Create, read, update, delete.
+
+
+internal class ProductService
+{
+
+    public void CreateProduct()
+    {
+        //Need to recieve the productname and price from other area.
+
+    }
+
+    public void UpdateProduct()
+    {
+
+    }
+
+    public void DeleteProduct()
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
